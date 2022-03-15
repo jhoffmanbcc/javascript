995fe1b8bd0282f10c3fef7ef029082a84278916
Just Hoff <jhoffmanbcc@gmail.com>
2022-03-15 02:14:51 -0700

    Update example

--- d.1/d.js
+++ d.1/d.js
@@ -10,13 +10,23 @@ alert('This is popping off!!');
 // a single line comment has two slashes
 /* A double line comment has a slash and asterisk */
 
-// Making Variables
 
-var a = "secret message";
-console.log(a);
+/* Now we will create a variable */
+/* Follow along with W3 schools on variables.. because W3 schools is amazing 
+https://www.w3schools.com/js/js_variables.asp.  */
 
-var tutorialGrade = 100;
-console.log(tutorialGrade);
+
+// we will create a couple variables 
+
+
+var instructor = 60;
+var rating = 40;
+var score = instructor + rating;
+console.log(score);
+
+
+
+
 
 
 
